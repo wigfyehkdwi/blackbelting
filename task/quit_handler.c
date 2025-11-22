f2d86c2b2d9cd8e9b9250cfe940f667a4a38b7eb
widfyehkdwi <amoigus42069@tutamail.com>
2025-11-22 13:00:28 -0500

    rename function

--- task/quit_handler.c
+++ task/quit_handler.c
@@ -1,15 +1,15 @@
 #include "../core/game.h"
 #include "quit_handler.h"
 
-static void handle_tick(game_task *self);
+static void handle_event(game_task *self);
 
 game_task *quit_handler() {
 	game_task *task = game_new_task();
-	task->on_event = handle_tick;
+	task->on_event = handle_event;
 	return task;
 }
 
-static void handle_tick(game_task *self) {
+static void handle_event(game_task *self) {
 	if (self->event->type == SDL_EVENT_QUIT) {
 		self->game->aborted = true;
 	}
