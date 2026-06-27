985fb1463e2cc53f533d51432705d25bb3ffbb7d
widfyehkdwi <amoigus42069@tutamail.com>
2026-06-27 12:55:06 -0400

    more configurable weapons

--- src/task/game/weapon.c
+++ src/task/game/weapon.c
@@ -6,10 +6,23 @@ enum attack_type {
 };
 
 typedef struct {
-	game_task *owner;
 	enum attack_type type;
+	char *sprite_path;
+	float scale;
+} weapon_type;
+
+typedef struct {
+	game_task *owner;
+	weapon_type *type;
 } weapon_data;
 
+static weapon_type sword = {
+	.type = MELEE,
+	.sprite_path = "res/game/sword.png",
+	.scale = 3
+};
+
+static int apply_weapon(game_task *self, weapon_type *type);
 static int handle_spawn(game_task *self);
 static void handle_tick(game_task *self);
 static void handle_event(game_task *self);
@@ -24,18 +37,28 @@ game_task *weapon(game_task *owner) {
 	weapon_data *data = calloc(sizeof(weapon_data), 1);
 	if (data == NULL) return NULL;
 	data->owner = owner;
+	data->type = &sword;
 	self->data = data;
 
 	return self;
 }
 
-static int handle_spawn(game_task *self) {
-	self->sprite = calloc(sizeof(game_sprite), 1);
+static int apply_weapon(game_task *self, weapon_type *type) {
+	weapon_data *data = self->data;
+	data->type = type;
+
+	if (self->sprite == NULL) self->sprite = new_game_sprite();
 	if (self->sprite == NULL) return -1;
-	self->sprite->texture = IMG_LoadTexture(self->game->renderer, "res/game/sword.png");
+	self->sprite->texture = IMG_LoadTexture(self->game->renderer, type->sprite_path);
 	if (self->sprite->texture == NULL) return -1;
 	SDL_SetTextureScaleMode(self->sprite->texture, SDL_SCALEMODE_PIXELART);
-	game_scale_sprite(self->sprite, 3);
+	game_scale_sprite(self->sprite, type->scale);
+	return 0;
+}
+
+static int handle_spawn(game_task *self) {
+	weapon_data *data = self->data;
+	if (apply_weapon(self, data->type)) return -1;
 	return 0;
 }
 
